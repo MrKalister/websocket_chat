2e3321d93048773de911c07b303c68806893e70d
Novikov Maxim <maxon.nowik@yandex.ru>
2024-04-17 21:23:52 +0200

    add websocket index and websocket_client

--- main.py
+++ main.py
@@ -29,4 +29,3 @@ if __name__ == '__main__':
     event_loop = asyncio.get_event_loop()
     event_loop.run_until_complete(start_server)
     event_loop.run_forever()
-
---
+++ websocket_client.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let websocketClient = new WebSocket("ws://localhost:12345")
+
+}, false);
