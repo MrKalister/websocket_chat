cf7a0c5b178313a94e20357da0092a16c9f2b554
Novikov Maxim <maxon.nowik@yandex.ru>
2024-04-17 21:24:47 +0200

    fix event_loop

--- main.py
+++ main.py
@@ -27,5 +27,5 @@ async def start_server():
 
 if __name__ == '__main__':
     event_loop = asyncio.get_event_loop()
-    event_loop.run_until_complete(start_server)
+    event_loop.run_until_complete(start_server())
     event_loop.run_forever()
